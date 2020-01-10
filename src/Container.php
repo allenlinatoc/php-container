b67647bd33a1536532f822a23f5730eb9906e556
Allen Linatoc <linatoc.allen@gmail.com>
2020-01-10 02:25:50 -0500

    Base changes for 1.0.0

---
+++ src/Container.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace Allenlinatoc;
+
+
+use Allenlinatoc\Exceptions\ContainerNotFoundException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+
+class Container extends \Pimple\Container implements ContainerInterface
+{
+
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return mixed Entry.
+     * @throws ContainerNotFoundException  No entry was found for **this** identifier.
+     */
+    public function get($id)
+    {
+        if (!$this->offsetExists($id))
+        {
+            throw new ContainerNotFoundException();
+        }
+        return $this->offsetGet($id);
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
+     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
+     */
+    public function has($id)
+    {
+        return $this->offsetExists($id);
+    }
+
+
+    public function set($id, $value)
+    {
+        $this->offsetSet($id, $value);
+    }
+
+    public function __get($id)
+    {
+        return $this->get($id);
+    }
+
+    public function __set($id, $value)
+    {
+        $this->set($id, $value);
+    }
+
+}
---
+++ src/Exceptions/ContainerNotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Allenlinatoc\Exceptions;
+
+use Throwable;
+
+class ContainerNotFoundException extends \Exception
+{
+    const CODE_DEFAULT = 0;
+
+    /**
+     * ContainerNotFoundException constructor.
+     * @param string $id        ID or name of container element not found
+     * @param int $code         (Optional) Exception
+     * @param Throwable|null $previous
+     */
+    public function __construct($id, $code = self::CODE_DEFAULT, Throwable $previous = null)
+    {
+        parent::__construct("Container element \"" . $id . "\" not found", $code, $previous);
+    }
+}
